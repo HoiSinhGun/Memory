7fba7da727176ab96dc1dbf5e0d3da7d361cb892
rgun <r.guenther.zh@gmail.com>
2020-06-04 03:46:19 +0200

    qwew

--- Memory/Program.cs
+++ Memory/Program.cs
@@ -6,7 +6,7 @@ namespace Memory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 2!");
         }
     }
 }
